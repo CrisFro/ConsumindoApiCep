655244b3bf6270edcc26b68d7ebbbf180ed2ab85
Cristiane Guimarães Fröhlich <cristiane_g_frohlich@hotmail.com>
2022-11-11 17:53:49 -0300

    Initial commit

---
+++ ICepApiService.cs
@@ -0,0 +1,10 @@
+﻿using Refit;
+
+namespace ConsumindoApiCep
+{
+    public interface ICepApiService //interface onde é chamada a API
+    {
+        [Get("/ws/{cep}/json")]
+        Task<CepResponse> GetAddressAsync(string cep);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+using Refit;
+
+namespace ConsumindoApiCep
+{
+    class MainClass
+    {
+        static async Task Main(string[] args) //asynk task - trabalha de forma assíncrona.
+        {
+            try
+            {
+                var cepClient = RestService.For<ICepApiService>("http://viacep.com.br"); //passando a interface que queremos mapear + host url
+                Console.WriteLine("Informe seu cep: ");
+
+                string cepInformado = Console.ReadLine().ToString(); //capturando cep digitado
+                Console.WriteLine("Consultando informações do CEP: " + cepInformado);
+
+                var address = await cepClient.GetAddressAsync(cepInformado); //criando chamada ao refit - await serve para que aguarde o processamento
+
+                Console.Write($"\nLogradouro:{address.Logradouro}\nBairro:{address.Bairro}\nCidade:{address.Localidade}\nIbge:{address.Ibge}");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro na consulta de cep: " + e.Message);
+            }
+        }
+    }
+
+
+}
